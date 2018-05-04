7516434cb34717ed34d6e84f007743e40da4785f
denra <denra82@gmail.com>
2018-05-04 17:01:58 +0200

    patient changes
    changes

--- webapp/src/app/Patients/patients.component.ts
+++ webapp/src/app/Patients/patients.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from 'angularfire2/firestore';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-patients',
@@ -7,7 +10,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PatientsComponent implements OnInit {
 
-  constructor() { }
+  patientnumber: string;
+  firstname: string;
+  lastname: string;
+  street: string;
+  zip: number;
+  city: string;
+  dateOfBirth: Date;
+  telephoneNumber: string;
+  gender: string;
+  maritalstatus: string;
+  dateregistrered: Date;
+  fullnamelocaldoctor: string;
+  clinicnumber: string;
+  streetlocaldoctor: string;
+  ziplocaldoctor: number;
+  citylocaldoctor: string;
+  phonenumber: string;
+
+  patient: AngularFirestoreCollection<any[]>;
+  patients: Observable<any[]>;
+
+  constructor(public db: AngularFirestore) {
+    db.firestore.settings({ timestampsInSnapshots: true});
+    this.patient = this.db.collection('Patients');
+    this.patients = this.patient.valueChanges();
+  }
 
   ngOnInit() {
   }
