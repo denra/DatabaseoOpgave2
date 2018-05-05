3341598a1e3647bd6bb606176bf0e3e436790398
CamillaME <info@camillame.dk>
2018-05-05 14:41:39 +0200

    Update staff

--- webapp/src/app/staff/staff.component.ts
+++ webapp/src/app/staff/staff.component.ts
@@ -27,24 +27,16 @@ export class StaffComponent implements OnInit {
   salaryScale: string;
 
   docID: string = this.randomStaffNumber();
+  id: string;
+
+  isVisibleCreate: boolean = true;
+  isVisibleUpdate: boolean = false;
 
   staffCol: AngularFirestoreCollection<any>;
   staffs: Observable<any[]>;
 
   constructor(private db: AngularFirestore) {
     db.firestore.settings({ timestampsInSnapshots: true });
-
-    // this.docID = "Hey";
-
-    //   firebase.database().ref('/posts').on('value', function(snapshot) {
-    //     console.log(snapshotToArray(snapshot));
-    // });
-
-    // db.collection("Staff").ref.get().then(function (querySnapshot) {
-    //   querySnapshot.forEach(function (doc) {
-    //     console.log(doc.id);
-    //   });
-    // });    
   }
 
   randomStaffNumber() {
@@ -104,6 +96,76 @@ export class StaffComponent implements OnInit {
         },
       ]
     });
+  }
+
+  onEdit(id, firstName, lastName, street, zip, city, gender, dateOfBirth, telephoneNumber, insuranceNumber, position, currentSalary, whenPaid, hoursPerWeek, permanentOrTemporary, salaryScale) {
+    this.id = id;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.street = street;
+    this.zip = zip;
+    this.city = city;
+    this.gender = gender;
+    this.dateOfBirth = dateOfBirth;
+    this.telephoneNumber = telephoneNumber;
+    this.insuranceNumber = insuranceNumber;
+    this.position = position;
+    this.currentSalary = currentSalary;
+    this.whenPaid = whenPaid;
+    this.hoursPerWeek = hoursPerWeek;
+    this.permanentOrTemporary = permanentOrTemporary;
+    this.salaryScale = salaryScale;
+
+    this.isVisibleCreate = false;
+    this.isVisibleUpdate = true;
+  }
+
+  onUpdate(id) {
+    this.staffCol.doc(id).update({
+      'FirstName': this.firstName,
+      "LastName": this.lastName,
+      "Address": {
+        "Street": this.street,
+        "Zip": this.zip,
+        "City": this.city
+      },
+      "Gender": this.gender,
+      "DateOfBirth": this.dateOfBirth,
+      "TelephoneNumber": this.telephoneNumber,
+      "InsuranceNumber": this.insuranceNumber,
+      "Position": this.position,
+      "CurrentSalary": this.currentSalary,
+      "WhenPaid": this.whenPaid,
+      "HoursPerWeek": this.hoursPerWeek,
+      "PermanentOrTemporary": this.permanentOrTemporary,
+      "SalaryScale": this.salaryScale,
+      "Qualifications": [
+        {
+          "Date": "25-2-2018",
+          "Institution": "UCN",
+          "Type": "U"
+        },
+        {
+          "Date": "25-2-2016",
+          "Institution": "BGJ",
+          "Type": "U"
+        },
+      ],
+      "WorkExperience": [
+        {
+          "StartDate": "25-2-2013",
+          "FinishDate": "25-2-2017",
+          "Position": "Nurse",
+          "Organization": "Aalborg Universitets Hospital"
+        },
+        {
+          "StartDate": "25-2-2011",
+          "FinishDate": "25-2-2012",
+          "Position": "Nurse",
+          "Organization": "Aalborg Universitets Hospital"
+        },
+      ]
+     });
   }
 
   ngOnInit() {
