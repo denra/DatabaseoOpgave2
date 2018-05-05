e7a7e613b2887b7e410cc6b6d888e8ba65d7d84e
denra <denra82@gmail.com>
2018-05-05 16:56:21 +0200

    changes

--- webapp/src/app/getstaffinward/getstaffinward.component.ts
+++ webapp/src/app/getstaffinward/getstaffinward.component.ts
@@ -3,26 +3,24 @@ import { AngularFireDatabase } from 'angularfire2/database';
 import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from 'angularfire2/firestore';
 import { Observable } from 'rxjs/Observable';
 
+interface Staff{
+  
+}
+
 @Component({
   selector: 'app-getstaffinward',
   templateUrl: './getstaffinward.component.html',
   styleUrls: ['./getstaffinward.component.css']
 })
 export class GetstaffinwardComponent implements OnInit {
-
-  staff: AngularFirestoreCollection<any[]>;
+  Staff;
+  private staff: AngularFirestoreCollection<Staff>;
   staffs: Observable<any[]>;
 
   constructor(public db: AngularFirestore) { 
     db.firestore.settings({ timestampsInSnapshots: true});
 
-    db.collection('Staff').where('AllocatedInWard', '==', 'Ward/1').get().then(function(querySnapshot) {
-      querySnapshot.ForEach(function(doc) {
-        console.log(doc.id, '=>', doc.Data());
-      })
-    }).catch(function(error){
-      console.log('Error in getting documents: ', error);
-    });
+    db.collection('Staff', ref => ref.where('AllocatedInWard', '==', 'Ward/1'));
   }
 
   ngOnInit() {
