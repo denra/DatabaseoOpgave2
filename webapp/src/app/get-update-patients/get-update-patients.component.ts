e37630bfe966063fbea92a22fcefc3f7c0b5ca15
denra <denra82@gmail.com>
2018-05-05 11:55:54 +0200

    Getting data fra databasen
    For nu dataen ud fra et document

--- webapp/src/app/get-update-patients/get-update-patients.component.ts
+++ webapp/src/app/get-update-patients/get-update-patients.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from 'angularfire2/firestore';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-get-update-patients',
@@ -7,7 +10,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GetUpdatePatientsComponent implements OnInit {
 
-  constructor() { }
+  patient;
+  constructor(public db: AngularFirestore) {
+    console.log('constructor');
+    
+    this.db.collection('Patients').doc('6iExg').ref.get().then((doc) =>{
+      if (doc.exists){
+        console.log('Document data: ', doc.data());
+        this.patient = doc.data();
+      }else{
+        console.log('No such document');
+      }
+    }).catch(function(error) {
+      console.log('Error in getting the document: ', error);
+    });
+
+   }
 
   ngOnInit() {
   }
