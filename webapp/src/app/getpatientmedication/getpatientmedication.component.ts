0637e09b3ed9ac4c55aefab80eec6b26f830359b
denra <denra82@gmail.com>
2018-05-06 18:04:54 +0200

    rapport om patientmedication

--- webapp/src/app/getpatientmedication/getpatientmedication.component.ts
+++ webapp/src/app/getpatientmedication/getpatientmedication.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from 'angularfire2/firestore';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-getpatientmedication',
@@ -7,9 +10,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GetpatientmedicationComponent implements OnInit {
 
-  constructor() { }
+  patientmedicationCol: AngularFirestoreCollection<any>;
+  patientmedications: Observable<any[]>;
+  thepatientmedication:Observable<any[]>;
+
+  patient: string;
+
+  constructor(public db: AngularFirestore) { 
+    db.firestore.settings({ timestampsInSnapshots: true});
+  }
+  seachPatientMedication() {
+    this.patientmedications = this.db.collection("PatientMedication", ref => ref.where('Patient', '==', this.patient))
+      .valueChanges();
+  }
 
   ngOnInit() {
+    this.patientmedicationCol = this.db.collection('PatientMedication');
+    this.patientmedications = this.patientmedicationCol.valueChanges();
+    this.thepatientmedication= this.patientmedicationCol.valueChanges();
   }
 
 }
