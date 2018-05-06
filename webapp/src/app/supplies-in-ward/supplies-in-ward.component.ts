08daac9ba272fcf173ddfe434660d30c812064c3
CamillaME <info@camillame.dk>
2018-05-06 19:18:14 +0200

    Report of supplies
    Har tilføjet WardNumber i alle tre tabeller - ellers vidste jeg ikke lige, hvordan jeg skulle lave det

--- webapp/src/app/supplies-in-ward/supplies-in-ward.component.ts
+++ webapp/src/app/supplies-in-ward/supplies-in-ward.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from 'angularfire2/firestore';
+import { Observable } from 'rxjs/Observable';
+import { combineLatest } from 'rxjs/observable/combineLatest';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-supplies-in-ward',
@@ -7,9 +12,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SuppliesInWardComponent implements OnInit {
 
-  constructor() { }
+  suppliesCol: AngularFirestoreCollection<any>;
+  supplies: Observable<any[]>;
+
+  wardCol: AngularFirestoreCollection<any>;
+  wards: Observable<any[]>;
+
+  wardRequistionCol: AngularFirestoreCollection<any>;
+  wardRequistions: Observable<any[]>;
+
+  combinedListCol: AngularFirestoreCollection<any>;
+  combinedList: Observable<any[]>;
+
+  constructor(private db: AngularFirestore) {
+    db.firestore.settings({ timestampsInSnapshots: true });
+  }
 
   ngOnInit() {
+    this.suppliesCol = this.db.collection('Supplies', ref => ref.where('WardNumber', '==', 1));
+    this.supplies = this.suppliesCol.valueChanges();
+
+    this.wardCol = this.db.collection('Ward', ref => ref.where('WardNumber', '==', 1));
+    this.wards = this.wardCol.valueChanges();
+
+    this.wardRequistionCol = this.db.collection('WardRequistion', ref => ref.where('WardNumber', '==', 1));
+    this.wardRequistions = this.suppliesCol.valueChanges();
+
+    this.combinedList = combineLatest<any[]>(this.supplies, this.wards, this.wardRequistions).pipe(
+      map(arr => arr.reduce((acc, cur) => acc.concat(cur))),
+    )
   }
 
 }
